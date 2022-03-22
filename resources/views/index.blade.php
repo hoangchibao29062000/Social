6496ed38319e6045f4e90d77769b79b37420fc80
duongan1405 <85775543+duongan1405@users.noreply.github.com>
2022-03-22 17:15:45 +0700

    add login

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    An Tesst
-    <table>
-        @foreach($post as $item)
-        <tr>
-            <td>{{$item->post_id}}</td>
-        </tr>
-        @endforeach
-    </table>
-</body>
-</html>
+
+    @extends('welcome')
+    
+    @section('content')
+    <div class="container">
+        <div class="login">
+            <div class="card" style="width: 20rem;">
+               <form>
+                   <!-- Tên Đăng Nhập -->
+                   <div class="m-3">
+                    
+                    <input type="text" class="form-control" placeholder="Nhập email hoặc số điện thoại"/>
+                   </div>
+                   <!-- Mật Khẩu -->
+                   <div class="m-3">
+                    
+                    <input type="password" class="form-control" placeholder="Nhập mật khẩu"/>
+                   </div>
+                   <!-- Nút Đăng Nhập -->
+                   <div class="m-3 text-center">
+                    <button type="submit" class="btn btn-primary form-control mb-3">Đăng nhập</button>
+                    <a href="">Quên mật khẩu</a>
+                   </div>
+                   <hr width="60%">
+                   <div class="m-3 text-center">
+                    <button type="submit" class="btn btn-success w-50 form-control mb-3">Tạo Tài Khoản</button>
+                   </div>
+               </form>
+            </div>
+        </div>
+    </div>
+    @endsection
+    
