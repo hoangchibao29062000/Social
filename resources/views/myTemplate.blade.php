53c9f9f7730573da5c0f90e356b4beff8799616a
hoangchibao29062000 <85774706+hoangchibao29062000@users.noreply.github.com>
2022-03-23 10:43:04 +0700

    fix

--- resources/views/myTemplate.blade.php
+++ resources/views/myTemplate.blade.php
@@ -56,22 +56,23 @@
    </style>
 </head>
 <body>
-@include('block.header')
-
+    @if (!empty($_SESSION['login']))
+        @include('block.header')
         <!-- Content -->
         <div class="row" style="margin-top:90px;margin-right: 15px;">
             <div class=" bg-light col-2 col-lg-2 col-xl-2 fixed-left" style="width:1000px">
                 @include('block.sidebar')
             </div>
             <div class="col-7 col-lg-7 col-xl-7">
-                @yield('login')
                 @yield('content')
             </div>
             <div class="col-3 col-lg-3 col-xl-3"> 
                 @include('block.messages')
             </div>
         </div>
-        
-
+    @else
+        @yield('login')
+    @endif
+   
 </body>
 </html>
