66cd856be76342c207e3c71fcbc4e4bb672a247f
duongan1405 <85775543+duongan1405@users.noreply.github.com>
2022-03-23 14:49:25 +0700

    update post

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,31 +7,52 @@
         <div class="col-1">
         <button class="btn btn-light " style="border-radius: 360px;"><img src="images/user.png" width="30" height="30" alt="" srcset=""></button>
         </div>
-        <div class="col-9">
+        <div class="col-11">
           <button type="button "class="btn btn-light text-left form-control" data-toggle="modal" data-target="#exampleModal" style="border-radius: 360px;">Hôm nay bạn thế nào? </button>
             <!-- Modal -->
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+                        <h5 class="modal-title" id="exampleModalLabel">Tạo Bài Viết</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
+                    <!-- Body Modal -->
                     <div class="modal-body">
-                    ...
+                        <div class="row">
+                            <!-- Avatar người tạo bài -->
+                            <div class="col-1">
+                                <img src="images/user.png" height="30" width="30"alt="" srcset="">
+                            </div>
+                            <div class="col-10 text-left">
+                                <div class="row">
+                                    <!-- Tên người tạo bài -->
+                                    <div class="col-12">
+                                        {{$_SESSION['login']['name']}}
+                                    </div>
+                                    <!-- Chọn chế độ của bài viết -->
+                                    <div class="col-12">
+                                    <select name="role">
+                                        <option value="private">Chỉ mình tôi</option>
+                                        <option value="public">Cộng đồng</option>
+                                    </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <textarea name="content" placeholder="Hôm nay bạn thế nào" cols="60" rows="10"></textarea>
+                        <p>Chọn ảnh</p>
+                        <input type="file" name="image"> 
                     </div>
+                    <!-- Nút Submit -->
                     <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-primary form-control">Đăng Bài Viết</button>
                     </div>
                 </div>
                 </div>
             </div>
-        </div>
-        <div class="col-2">
-          <button class="btn btn-primary">Đăng bài</button>
         </div>
         <hr width="100%">
       </div>
